be019cb1bfc2fe56f1ff1d6e62570018dd3709d1
me0wmix <7215827+CatLover91@users.noreply.github.com>
2017-10-23 00:38:07 -0400

    doxygen was already there :v

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -1,31 +1,26 @@
 require 'package'
 
 class Doxygen < Package
-  description 'Generate documentation for several programming languages'
-  homepage 'http://doxygen.org/'
+  description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources'
+  homepage 'http://www.stack.nl/~dimitri/doxygen/'
   version '1.8.13'
-  source_url 'https://github.com/doxygen/doxygen/archive/Release_1_8_13.tar.gz'
-  source_sha256 ''
+  source_url 'https://github.com/doxygen/doxygen/archive/Release_1_8_13.zip'
+  source_sha256 '1a1f67106c7e7642b79884f02faaf753d2e42be1ebac04083c173224123783b5'
 
-  binary_url ({ })
-  binary_sha256 ({ })
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
 
-  depends_on 'cmake' => :build
+  depends_on 'cmake'
+  depends_on 'unzip'
 
   def self.build
-    system 'mkdir out'
-    Dir.chdir 'out' do
-      args = "-DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DCMAKE_INSTALL_LIBDIR=#{CREW_DEST_LIB_PREFIX}"
-      # args << "-Dbuild_wizard=ON" if build.with? "qt"
-      
-      system "cmake", "..", *args
-      system "cmake --build . --config Release --target install"
-    end
+    system "cmake ."
+    system "make"
   end
-    
+
   def self.install
-    Dir.chdir 'out' do
-	    system "cmake -DCMAKE_INSTALL)PREFIX=#{CREW_DEST_PREFIX} -DCMAKE_INSTALL_LIBDIR=#{CREW_DEST_LIB_PREFIX} -P cmake_install.cmake"
-    end
+    system "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 end
